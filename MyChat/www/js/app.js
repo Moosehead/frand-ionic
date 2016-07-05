f88aa84960faa390f090daca323859194f1b4bde
Moosa Baquie <mbaquie@evertz.com>
2016-07-05 15:27:03 -0400

    chat login features

--- MyChat/www/js/app.js
+++ MyChat/www/js/app.js
@@ -1,13 +1,15 @@
 // Ionic Starter App
 
+var firebaseUrl = "https://frand-6ddcc.firebaseio.com";
+document.addEventListener("deviceready", onDeviceReady, false);
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'firebase', 'angularMoment', 'mychat.controllers', 'mychat.services'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope, $location, Auth, $ionicLoading) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -21,6 +23,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       StatusBar.styleDefault();
     }
   });
+  $rootScope.logout = function () {
+    console.log("Logging out from the app");
+  }
+
+  angular.module('mychat.services', ['firebase'])
+    .factory("Auth", ["$firebaseAuth", "$rootScope",
+      function ($firebaseAuth, $rootScope) {
+        var ref = new Firebase(firebaseUrl);
+        return $firebaseAuth(ref);
+      }]);
+
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -35,20 +48,48 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     .state('tab', {
     url: '/tab',
     abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+    templateUrl: 'templates/tabs.html',
+      resolve: {
+        // controller will not be loaded until $requireAuth resolves
+        // Auth refers to our $firebaseAuth wrapper in the example above
+        "currentAuth": ["Auth",
+          function (Auth) {
+            // $requireAuth returns a promise so the resolve waits for it to complete
+            // If the promise is rejected, it will throw a $stateChangeError (see above)
+            return Auth.$requireAuth();
+          }]
+      }
+    })
+
+    // State to represent Login View
+    .state('login', {
+      url: "/login",
+      templateUrl: "templates/login.html",
+      controller: 'LoginCtrl' ,
+      resolve: {
+        // controller will not be loaded until $waitForAuth resolves
+        // Auth refers to our $firebaseAuth wrapper in the example above
+        "currentAuth": ["Auth",
+          function (Auth) {
+            // $waitForAuth returns a promise so the resolve waits for it to complete
+            return Auth.$waitForAuth();
+          }]
+      }
+    })
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+    // Each tab has its own nav history stack:
+
+    .state('tab.rooms', {
+      url: '/rooms',
+      views: {
+        'tab-rooms': {
+          templateUrl: 'templates/tab-rooms.html',
+          controller: 'RoomsCtrl'
+        }
       }
-    }
-  })
+    })
 
   .state('tab.chats', {
       url: '/chats',
@@ -59,6 +100,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
       }
     })
+
     .state('tab.chat-detail', {
       url: '/chats/:chatId',
       views: {
@@ -79,7 +121,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+// if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
 
 });
--- MyChat/www/js/controllers.js
+++ MyChat/www/js/controllers.js
@@ -1,28 +1,114 @@
-angular.module('starter.controllers', [])
+// angular.module('starter.controllers', [])
+//
+// .controller('DashCtrl', function($scope) {
+//
+//
+//
+// })
+//
+// .controller('ChatsCtrl', function($scope, Chats) {
+//   // With the new view caching in Ionic, Controllers are only called
+//   // when they are recreated or on app start, instead of every page change.
+//   // To listen for when this page is active (for example, to refresh data),
+//   // listen for the $ionicView.enter event:
+//   //
+//   //$scope.$on('$ionicView.enter', function(e) {
+//   //});
+//
+//   $scope.chats = Chats.all();
+//   $scope.remove = function(chat) {
+//     Chats.remove(chat);
+//   };
+// })
+//
+// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//   $scope.chat = Chats.get($stateParams.chatId);
+// })
+//
+// .controller('AccountCtrl', function($scope) {
+//   $scope.settings = {
+//     enableFriends: true
+//   };
+//
+//
+// });
 
-.controller('DashCtrl', function($scope) {})
+angular.module('mychat.controllers', [])
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  .controller('LoginCtrl', function ($scope, $ionicModal, $state) {
+    console.log('Login Controller Initialized');
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+    $ionicModal.fromTemplateUrl('templates/signup.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    $scope.createUser = function (user) {
+      console.log("Create User Function called");
+      if (user && user.email && user.password && user.displayname) {
+        $ionicLoading.show({
+          template: 'Signing Up...'
+        });
+        auth.$createUser({
+          email: user.email,
+          password: user.password
+        }).then(function (userData) {
+          alert("User created successfully!");
+          ref.child("users").child(userData.uid).set({
+            email: user.email,
+            displayName: user.displayname
+          });
+          $ionicLoading.hide();
+          $scope.modal.hide();
+        }).catch(function (error) {
+          alert("Error: " + error);
+          $ionicLoading.hide();
+        });
+      } else
+        alert("Please fill all details");
+    }
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+
+
+    $scope.signIn = function () {
+      if (user && user.email && user.pwdForLogin) {
+        $ionicLoading.show({
+          template: 'Signing In...'
+        });
+        auth.$authWithPassword({
+          email: user.email,
+          password: user.pwdForLogin
+        }).then(function (authData) {
+          console.log("Logged in as:" + authData.uid);
+          ref.child("users").child(authData.uid).once('value', function (snapshot) {
+            var val = snapshot.val();
+            // To Update AngularJS $scope either use $apply or $timeout
+            $scope.$apply(function () {
+              $rootScope.displayName = val;
+            });
+          });
+          $ionicLoading.hide();
+          $state.go('tab.rooms');
+        }).catch(function (error) {
+          alert("Authentication failed:" + error.message);
+          $ionicLoading.hide();
+        });
+      } else
+        alert("Please enter email and password both");
+    }
+  })
+
+
+  .controller('ChatCtrl', function ($scope, Chats) {
+    console.log("Chat Controller initialized");
+    $scope.chats = Chats.all();
+
+  })
+
+  .controller('RoomsCtrl', function ($scope, Rooms) {
+  console.log("Rooms Controller initialized");
+  $scope.rooms = Rooms.all();
+
+
+  });
